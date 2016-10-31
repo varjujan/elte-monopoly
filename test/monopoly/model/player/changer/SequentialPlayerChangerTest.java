d337e874b39ad8b2cba8e2d7c7e08ee3bc983ec4
VarjuJanos <varjujanos2@gmail.com>
2016-10-31 16:42:02 +0100

    Dice roll test

--- test/monopoly/model/player/changer/SequentialPlayerChangerTest.java
+++ test/monopoly/model/player/changer/SequentialPlayerChangerTest.java
@@ -1,6 +1,9 @@
 package monopoly.model.player.changer;
 
+import monopoly.model.dice.MultipleDiceResult;
+import monopoly.model.dice.SingleDiceResult;
 import monopoly.model.player.Player;
+import monopoly.model.player.State;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -22,6 +25,7 @@ public class SequentialPlayerChangerTest {
         Player player = mock(Player.class);
         List<Player> players = mock(List.class);
 
+        when(player.getState()).thenReturn(State.InGame);
         when(players.get(anyInt())).thenReturn(player);
         when(players.size()).thenReturn(1);
 
@@ -32,7 +36,8 @@ public class SequentialPlayerChangerTest {
             assertEquals(player, spChanger.currentPlayer());
         }
 
-        verify(players, times(10)).get(0);
+        verify(players, times(20)).get(0);
+        verify(player, times(5)).setDiceRollsLeft(anyInt());
     }
 
     @Test
@@ -47,6 +52,10 @@ public class SequentialPlayerChangerTest {
         when(players.get(2)).thenReturn(player2);
         when(players.size()).thenReturn(3);
 
+        when(player0.getState()).thenReturn(State.InJail);
+        when(player1.getState()).thenReturn(State.InJail);
+        when(player2.getState()).thenReturn(State.InJail);
+
         SequentialPlayerChanger spChanger = new SequentialPlayerChanger(players);
 
         assertEquals(player0, spChanger.currentPlayer());
@@ -59,7 +68,76 @@ public class SequentialPlayerChangerTest {
         assertEquals(player1, spChanger.nextPlayer());
         assertEquals(player1, spChanger.currentPlayer());
 
-        verify(players, times(9)).get(anyInt());
+        verify(players, times(13)).get(anyInt());
+        verify(player0, times(0)).setDiceRollsLeft(anyInt());
+        verify(player1, times(0)).setDiceRollsLeft(anyInt());
+        verify(player2, times(0)).setDiceRollsLeft(anyInt());
+    }
+
+    @Test
+    public void handleRollWithDifferentDices() {
+        Player player = mock(Player.class);
+        List<Player> players = mock(List.class);
+        List<SingleDiceResult> sdResList = mock(List.class);
+
+        SingleDiceResult sdRes1 = mock(SingleDiceResult.class);
+        SingleDiceResult sdRes2 = mock(SingleDiceResult.class);
+        MultipleDiceResult mdRes = mock(MultipleDiceResult.class);
+
+        when(sdResList.get(0)).thenReturn(sdRes1);
+        when(sdResList.get(1)).thenReturn(sdRes2);
+
+        when(sdRes1.getResult()).thenReturn(1);
+        when(sdRes2.getResult()).thenReturn(2);
+
+        when(mdRes.getResult()).thenReturn(sdResList);
+
+        when(players.get(0)).thenReturn(player);
+        when(players.size()).thenReturn(1);
+
+        SequentialPlayerChanger spChanger = new SequentialPlayerChanger(players);
+
+        assertEquals(true, spChanger.handleRoll(mdRes));
+
+        verify(player, times(0)).decreaseDiceRollsLeft();
+        verify(player, times(0)).setPosition(anyInt());
+    }
+
+    @Test
+    public void handleRollWithEqualDices() {
+        Player player = mock(Player.class);
+        List<Player> players = mock(List.class);
+        List<SingleDiceResult> sdResList = mock(List.class);
+
+        SingleDiceResult sdRes1 = mock(SingleDiceResult.class);
+        SingleDiceResult sdRes2 = mock(SingleDiceResult.class);
+        MultipleDiceResult mdRes = mock(MultipleDiceResult.class);
+
+        when(sdResList.get(0)).thenReturn(sdRes1);
+        when(sdResList.get(1)).thenReturn(sdRes2);
+
+        when(sdRes1.getResult()).thenReturn(1);
+        when(sdRes2.getResult()).thenReturn(1);
+
+        when(mdRes.getResult()).thenReturn(sdResList);
+
+        when(players.get(0)).thenReturn(player);
+        when(players.size()).thenReturn(1);
+
+        SequentialPlayerChanger spChanger = new SequentialPlayerChanger(players);
+
+        when(player.getDiceRollsLeft()).thenReturn(2);
+        assertEquals(true, spChanger.handleRoll(mdRes));
+
+        when(player.getDiceRollsLeft()).thenReturn(1);
+        assertEquals(true, spChanger.handleRoll(mdRes));
+
+        when(player.getDiceRollsLeft()).thenReturn(0);
+        assertEquals(false, spChanger.handleRoll(mdRes));
+
+        verify(player, times(3)).decreaseDiceRollsLeft();
+        verify(player, times(1)).setPosition(anyInt());
+        verify(player, times(1)).setState(anyObject());
     }
 
 }
